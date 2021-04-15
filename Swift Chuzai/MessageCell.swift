b949431e68f305581c1c22d2fe915d14ceb9e95d
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-04-16 00:34:00 +0900

    Make custom cell

--- Swift Chuzai/MessageCell.swift
+++ Swift Chuzai/MessageCell.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class MessageCell: UITableViewCell {
 
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var backView: UIView!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
