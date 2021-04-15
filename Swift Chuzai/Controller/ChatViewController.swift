a0029e007e03eff4771d600f10bbcf28142c2bec
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-04-15 22:12:49 +0900

    Add ChatVC

---
+++ Swift Chuzai/Controller/ChatViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ChatViewController.swift
+//  Swift Chuzai
+//
+//  Created by 岡真也 on 2021/04/15.
+//
+
+import UIKit
+import Firebase
+import SDWebImage
+
+class ChatViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
