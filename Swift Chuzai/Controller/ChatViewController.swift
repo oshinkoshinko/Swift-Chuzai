7812ffa215b02809d7b7542fb3466a2ac7e40ad8
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-04-15 22:55:56 +0900

    Add Message model

--- Swift Chuzai/Controller/ChatViewController.swift
+++ Swift Chuzai/Controller/ChatViewController.swift
@@ -9,18 +9,81 @@ import UIKit
 import Firebase
 import SDWebImage
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextField: UITextField!
+    let db = Firestore.firestore()
     
+    var roomName = String()
+    var imageString = String()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        //userimageにurlが入っていたら
+        if UserDefaults.standard.object(forKey: "userimage") != nil{
+            
+            //ImageStringにurlを文字列型で格納
+            imageString = UserDefaults.standard.object(forKey: "userimage") as! String
+            
+        }
+        //ルーム名なし==全体チャット
+        if roomName == ""{
+            
+            roomName = "All"
+            
+        }
+        
+        self.navigationItem.title = roomName
+        
 
-        // Do any additional setup after loading the view.
     }
     
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    @IBAction func send(_ sender: Any) {
+        
+        if let messageBody = messageTextField.text,let sender = Auth.auth().currentUser?.email{
+            
+            //Firebase内のデータを辞書型で取得
+            db.collection(roomName).addDocument(data: ["sender":sender,"body":messageBody,"imageString":imageString,"date":Date().timeIntervalSince1970]) { (error) in
+                
+                if error != nil{
+                    
+                    print(error.debugDescription)
+                    return
+                    
+                }
+                
+                //非同期処理
+                DispatchQueue.main.async{
+                    //送信後はフォームを空にする
+                    self.messageTextField.text = ""
+                    //キーボードを閉じる
+                    self.messageTextField.resignFirstResponder()
+                }
+            }
+            
+        }
+        
+    }
+    
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
---
+++ Swift Chuzai/Model/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Swift Chuzai
+//
+//  Created by 岡真也 on 2021/04/15.
+//
+
+import Foundation
+
+struct Message {
+    
+    
+    
+}
