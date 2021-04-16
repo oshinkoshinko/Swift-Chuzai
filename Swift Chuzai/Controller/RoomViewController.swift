9732b6c18a1c2fc8e9a30ff8787360b2c3404d3d
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-04-16 10:54:55 +0900

    Add RoomModel

--- Swift Chuzai/Controller/RoomViewController.swift
+++ Swift Chuzai/Controller/RoomViewController.swift
@@ -6,15 +6,52 @@
 //
 
 import UIKit
+import ViewAnimator //tableviewのアニメーション用
 
-class RoomViewController: UIViewController {
+class RoomViewController: UIViewController,UITableViewDataSource,UITableViewDelegate{
 
+    
+    var roomNameArray = ["🇺🇸アメリカ","🇨🇳中国","🇦🇺オーストラリア","🇹🇭タイ","🇨🇦カナダ","🇬🇧イギリス","🇧🇷ブラジル","🇩🇪ドイツ","🇫🇷フランス"]
+    
+    var roomImageStringArray = ["0","1","2","3","4","5","6","7","8","9",]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isHidden = true
+        
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        tableView.isHidden = false
+        
+        let animation = [AnimationType.vector(CGVector(dx: 0, dy: 30))]
+        UIView.animate(views: tableView.visibleCells, animations:animation,completion:nil)
+        
+    }
+    
+    //セルの数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return roomNameArray.count
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        <#code#>
+    }
 
     /*
     // MARK: - Navigation
---
+++ Swift Chuzai/Model/RoomModel.swift
@@ -0,0 +1,8 @@
+//
+//  RoomModel.swift
+//  Swift Chuzai
+//
+//  Created by 岡真也 on 2021/04/16.
+//
+
+import Foundation
