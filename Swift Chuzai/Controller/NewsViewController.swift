d7c1227bf067bd16865caf5add8df98547dc320e
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-04-16 18:05:40 +0900

    Add NewsVC

---
+++ Swift Chuzai/Controller/NewsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  NewsViewController.swift
+//  Swift Chuzai
+//
+//  Created by 岡真也 on 2021/04/16.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    //ログアウト
+    @IBAction func logout(_ sender: Any) {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
