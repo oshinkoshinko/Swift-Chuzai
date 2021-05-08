35d5458d2b2454ff7c9b1328fbaf66f913e4701e
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-05-08 17:32:11 +0900

    Add loadUserData func to UserListVC

--- Swift Chuzai/Controller/UserListViewController.swift
+++ Swift Chuzai/Controller/UserListViewController.swift
@@ -6,12 +6,19 @@
 //
 
 import UIKit
+import Firebase
+import SDWebImage
 
 class UserListViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
     
 
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    //構造体User型が入る配列
+    var users:[User] = []
+    
+    let db = Firestore.firestore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,20 +29,81 @@ class UserListViewController: UIViewController, UICollectionViewDataSource, UICo
         
         self.navigationItem.title = "すべてのユーザー"
         
-        reloadData()
+        loadUserData()
         // Do any additional setup after loading the view.
     }
     
-
-    func reloadData(){
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.isNavigationBarHidden = false
         
         
+    }
+    
+    
+    func loadUserData(){
         
+        db.collection("User").addSnapshotListener { [self] (snapShot, error) in
+            
+            self.users = []
+            
+            if error != nil{
+                
+                print(error.debugDescription)
+                return
+                
+            }
+            
+            print("あいうえおおおおおお")
+            //firebaseのsnapShotにデータ入っている documentはその塊
+            if let snapShotDoc = snapShot?.documents{
+                
+                print("かきくけこおおおおお")
+                print(snapShotDoc)
+                //for文
+                for doc in snapShotDoc{
+                    
+                    //doc.dataでbodyやdateが入る
+                    let data = doc.data()
+                                        
+                    //必要なデータを取得
+                    if let userName = data["userName"] as? String,let imageString = data["imageString"] as? String, let uid = data["uid"] as? String, let country = data["country"] as? String{
+                                                
+                        //構造体Userにセットでデータを格納
+                        let newUser = User(userName: userName, imageString: imageString, uid: uid, country: country)
+                        
+                        self.users.append(newUser)
+                        
+                        DispatchQueue.main.async {
+                            
+                            //TableViewにメッセージを取得
+                            self.collectionView.reloadData()
+                            let indexPath = IndexPath(row: self.users.count - 1, section: 0)
+ 
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+        
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        
+        return 1
         
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        
+        return users.count
+        
     }
     
     //セルの中身
@@ -44,7 +112,7 @@ class UserListViewController: UIViewController, UICollectionViewDataSource, UICo
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
         
         let contentImageView = cell.contentView.viewWithTag(1) as! UIImageView
-        
+        contentImageView.sd_setImage(with: URL(string: users[indexPath.row].imageString), completed: nil)
         
         
         return cell
