b3db20f63811404b7b638d8ef87e9ca5fc4c9e5f
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-05-20 13:05:08 +0900

    Add SideMenuVC

---
+++ Swift Chuzai/Controller/SideMenuViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SideMenuViewController.swift
+//  Swift Chuzai
+//
+//  Created by 岡真也 on 2021/05/20.
+//
+
+import UIKit
+import SideMenu
+
+class SideMenuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
